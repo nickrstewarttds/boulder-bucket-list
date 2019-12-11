2f628698424300aa045c97f7be41c28f3202bf50
j97b <jamesbarron97@gmail.com>
2019-12-11 14:58:37 +0000

    first commit

---
+++ src/test/java/com/bae/BoulderBucketListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoulderBucketListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

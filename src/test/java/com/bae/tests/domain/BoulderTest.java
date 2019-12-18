927c23f767e5bce4d3d0ae4e71af15925e40f1d3
j97b <jamesbarron97@gmail.com>
2019-12-18 15:48:26 +0000

    Getter unit tests added

---
+++ src/test/java/com/bae/tests/domain/BoulderTest.java
@@ -0,0 +1,47 @@
+package com.bae.tests.domain;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bae.persistence.domain.Boulder;
+
+
+public class BoulderTest {
+	
+	private Date testAttemptDate = new Date(2001-01-01);
+	private Date testCompletionDate = new Date(2001-01-01);
+	private Boulder testBoulder = new Boulder("testName", "testLocation", "testGrade","testStatus",testAttemptDate,testCompletionDate);
+
+	@Test
+	public void getNameTest() {
+		Assert.assertEquals("testName", testBoulder.getName());
+	}
+	
+	@Test
+	public void getLocationTest() {
+		Assert.assertEquals("testLocation", testBoulder.getLocation());
+	}
+	
+	@Test
+	public void getGradeTest() {
+		Assert.assertEquals("testGrade", testBoulder.getGrade());
+	}
+	
+	@Test
+	public void getStatusTest() {
+		Assert.assertEquals("testStatus", testBoulder.getStatus());
+	}
+	
+	@Test
+	public void getAttemptDateTest() {
+		Assert.assertEquals(testAttemptDate, testBoulder.getAttemptDate());
+	}
+	
+	@Test
+	public void getCompletionDateTest() {
+		Assert.assertEquals(testCompletionDate, testBoulder.getCompletionDate());
+	}
+
+}

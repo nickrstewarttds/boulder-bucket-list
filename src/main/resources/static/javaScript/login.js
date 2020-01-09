0f976df2010930dc63516a4e8049b48e7cf53488
j97b <jamesbarron97@gmail.com>
2020-01-09 15:27:08 +0000

    webstorage added to login, should now work

--- src/main/resources/static/javaScript/login.js
+++ src/main/resources/static/javaScript/login.js
@@ -2,16 +2,19 @@ function makeUsersTable() {
     axios.get("/userApp/user")
         .then(response => {
             response.data.forEach(user => {
-            let table = document.getElementById('names');
-            let tr = document.createElement('tr');
-            let td = document.createElement('td');
-            tr.setAttribute('id',user.id);
-            tr.setAttribute('class',"nameRows");
-            tr.addEventListener('click', () => window.location = "html/bucketList.html");
-            let name = user.name.toString();
+                let table = document.getElementById('names');
+                let tr = document.createElement('tr');
+                let td = document.createElement('td');
+                tr.setAttribute('id',user.id);
+                tr.setAttribute('class',"nameRows");
+                tr.addEventListener('click', () => {
+                    localStorage.setItem("userID",user.id);
+                    window.location = "html/bucketList.html";
+                });
+                let name = user.name.toString();
                 td.innerText = name.charAt(0).toUpperCase() + name.substring(1);
-            table.appendChild(tr);
-            tr.appendChild(td);
+                table.appendChild(tr);
+                tr.appendChild(td);
             });
         }).catch(err => console.error(err));
 }

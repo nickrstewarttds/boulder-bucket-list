e97a33c8956d514dd44495cd0e3f209b117a9096
j97b <58259206+j97b@users.noreply.github.com>
2020-01-19 20:06:44 +0000

    Delete Constants.java

--- src/test/java/com/bae/tests/selenium/constants/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.bae.tests.selenium.constants;
-
-public class Constants {
-    public static final String PATH = "chromedriver";
-    public static final String PROPERTY = "webdriver.chrome.driver";
-    public static final String HOST = "http://localhost:";
-    public static final String INDEX = "index.html";
-    public static final String BUCKET_LIST = "html/bucketList.html";
-}
